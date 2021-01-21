28f0f20ebd24f65c4959efeb1e5f1f552d2292fb
Stephen Aranda <aranda.stephen88@gmail.com>
2021-01-21 17:52:16 -0600

    Completed tasks 4,5, and 6. completed the Instructor class, Student class, and ProjectManager class

--- index.js
+++ index.js
@@ -164,7 +164,26 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian {
+   constructor(childAttributes)
+   {
+     super(achildeAtributes);
+     this.specialty = childeAttributes.specialty;
+     this.favLanguage = childeAtributes.favLanguage;
+     this.catchPhrase = childeAttributes.catchPhrase;
+   }
+
+   demo(subject)
+   {
+     return `Today we are learning about ${subject}`;
+
+   }
+
+   grad(student,subject)
+   {
+     return `${student.name} receives a perfect score on ${subject}`;
+
+   }
 
  }
   /*
@@ -182,7 +201,30 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+ class Student extends Lambdasian {
+   construct(studentAttributes)
+   {
+     super(studentAttributes);
+     this.previousBackground = studentAttributes.previousBackground;
+     this.className = studentAttributes.className;
+     this.favSubjects = student.favSubjects;
+   }
+
+   listSubjects()
+   {
+     return `Loving ${this.favSubjects}!`;
+   }
+
+   PRAssignment(subject)
+   {
+     return `${this.name} has submitted a PR for ${subject}`;
+
+   }
+
+   sprintChallenge(subject)
+   {
+     return `${this.name} has begun sprint challenge of ${subject}`;
+   }
      
  }
   
@@ -199,8 +241,23 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor{
+     constructor(instructorAttributes)
+     {
+       super(instructorAttributes);
+       this.gradClassName = instructorAttributes.gradClassName;
+       this.favInstructor = instructorAttributes.favInstructor;
+     }
+
+     standUp(slackChannel)
+     {
+       return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+     }
+
+     debugsCode(student, subject)
+     {
+       return `${this.name} debugs ${student.name}'s code on ${subject}`;
+     }
  }
   /*
     STRETCH PROBLEM (no tests!)
